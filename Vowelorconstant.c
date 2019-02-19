892ebabcfb72dbbdd7fa3fa197c098916772ecb1
RUKESH99 <47105814+RUKESH99@users.noreply.github.com>
2019-02-19 07:32:44 +0530

    Create Vowelorconstant.c

---
+++ Vowelorconstant.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main(void)
+{
+	char x;
+	scanf("%c",x);
+	if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U')
+	{
+printf("Vowel");
+}
+else
+{
+printf("Consonant");
+}
+	return 0;
+}
