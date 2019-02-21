7b52bfe265c97df1cfa666e811a772286df9f068
RUKESH99 <47105814+RUKESH99@users.noreply.github.com>
2019-02-21 15:42:07 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,rev=0,temp;
+printf("check palindrome or not");
+scanf("%d",&n);
+temp=n;
+while(temp!=0)
+{
+rev=rev*10;
+rev=rev+temp%10;
+rev=temp/10;
+}
+if(n==rev)
+{
+printf("it is a palindrome);
+}elseif
+{
+printf("it is not a palindrome);
+}
+getch();
+}
+
