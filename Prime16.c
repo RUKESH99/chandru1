56637d93e3c4324bb58fd8464f82db28fdf9913a
RUKESH99 <47105814+RUKESH99@users.noreply.github.com>
2019-02-25 06:50:40 +0530

    Create Prime16.c

---
+++ Prime16.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+int start,end,i,j,c;
+scanf("%d%d",&start,&end);
+for(i=start;i<end;i++)
+{
+    c=0;
+	for(j=2;j<=i/2;++j)
+	{
+		if(i%j==0)
+		{
+		c=1;
+		break;
+		}
+	}
+	if(c==0)
+	{
+	printf("%d\n",i);
+	}
+}
+return 0;
+}
+© 2019 GitHub, Inc.
