a820970bd16e980adcf7e0afe396d0dc458e8089
RUKESH99 <47105814+RUKESH99@users.noreply.github.com>
2019-02-27 16:16:07 +0530

    Create Rarithemic21.c

---
+++ Rarithemic21.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<conio.h>
+
+#include<math.h>
+int main() 
+{
+	int a,b,c,d;
+	int A,B,C,D;
+	printf("\n enter the number of terms:");
+	scanf("%d",&a);
+	printf("\n enter the start value:");
+	scanf("%d",&b);
+	printf("\n enter the difference:");
+	scanf("%d",&c);
+	A=a-1;
+	C=A*c;
+	B=2*b;
+	D=B+C;
+	D=D*a/2;
+	printf("\n%d",d);
+	return 0;
+}
