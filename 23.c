775b8862690f0ff87a3334e8870e91bd4552bf97
RUKESH99 <47105814+RUKESH99@users.noreply.github.com>
2019-03-14 12:30:12 +0530

    Create 23.c

---
+++ 23.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int array[100], min, n, i, location = 1;
+ 
+  printf("Enter the number of elements in array\n");
+  scanf("%d", &n);
+ 
+  printf("Enter %d integers\n", n);
+ 
+  for (i = 0; i < n; i++)
+    scanf("%d", &array[i]);
+ 
+  min = array[0];
+ 
+  for (i = 1; i < n; i++)
+  {
+    if (array[i] < min)
+    {
+       min  = array[i];
+       location = i+1;
+    }
+  }
+ 
+  printf("Minimum element is present at location %d and it's value is %d.\n", location, min);
+  return 0;
+}
+© 2019 GitHub, Inc.
