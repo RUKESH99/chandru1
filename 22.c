8ca327bf51f081966a059d333ac3a3a7455ba93c
RUKESH99 <47105814+RUKESH99@users.noreply.github.com>
2019-03-14 12:26:31 +0530

    Create 22.c

---
+++ 22.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+int main() 
+{
+int arr[10],i,n,max=0;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+	scanf("%d",&arr[i]);
+}
+for(i=0;i<n;i++)
+{
+	if(max<arr[i])
+	{
+		max=arr[i];
+	}
+}
+printf("%d",max);
+getch();
+}
